25c01fc0beeef5dc201b0e6577ff0a038ef4e5f7
Ethan Brown <eb934053@gmail.com>
2025-04-28 09:22:39 -0500

    Comments describing the listing process for types

--- dmsapp/views/type.py
+++ dmsapp/views/type.py
@@ -10,8 +10,16 @@ class TypeSerializer(serializers.ModelSerializer):
 class TypeViewSet(viewsets.ViewSet):
     def list(self, request):
         try:
+            # Queries database with the ORM to list all types 
             types = Type.objects.all()
+
+            # Serializes the list of types into JSON
             serializer = TypeSerializer(types, many=True)
+
+            # Returns the JSON string in a response to the client
             return Response(serializer.data, status=status.HTTP_200_OK)
+        # All authenticated users are authorized for this response
         except Exception as ex:
+            # If there is any issue with a request to get the types, it is a server error
             return Response(f"There was an error serializing the types data: {ex.args[0]}", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
